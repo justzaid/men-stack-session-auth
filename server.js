bfe8de25d96d67aa0203009966f4fe47d0e6625e
justzxid <zaidmk96@gmail.com>
2025-02-02 14:37:48 +0300

    Creating first commit before further progress

---
+++ server.js
@@ -0,0 +1,21 @@
+// Require
+
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const morgan = require('morgan')
+const port = process.env.PORT ? process.env.PORT : '3000'
+
+// Creates a connection in MongoDB Database
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () => {
+    console.log('hello')
+    console.log(`Connected to MongoDB Database ${mongoose.connection.name}`)
+})
+
+
+app.listen(port, () => {
+    console.log(`The express app is ready on port ${port}`)
+})
